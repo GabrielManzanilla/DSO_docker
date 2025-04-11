08ffe8a1da94f0bbdd8738a8a20fa7a4dadd15d5
GabrielManzanilla <gabriel29444@gmail.com>
2025-04-11 13:15:43 -0600

    Add script to create tutorial and moodle-container

---
+++ tutorial-moodle.sh
@@ -0,0 +1,8 @@
+## Creacion del contenedor del tutorial
+docker run -d -p 8081:80 docker/getting-started
+
+# Levantar el servicio con Docker Compose
+docker-compose up -d
+
+# Mostrar contenedores activos
+docker ps
